056417d9f474212b5368df77623e51bb0a084d58
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-21 19:01:15 -0600

    Create initial player script

---
+++ Scripts/Player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed: int = 50
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("up_down") - Input.get_action_strength("ui_up")
+	
+	if 1 == abs(direction.x) and 1 == abs(direction.y):
+		direction = direction.normalized()
+	
+	var movement = speed * direction * delta
+	
+	move_and_collide(movement)
+	
